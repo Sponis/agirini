4ff095755e8f3d5ba9b7a95264b36051adbf330b
Sponis <sponis@gmail.com>
2014-01-17 21:11:20 +0200

    prwtes allages

---
+++ templates/rt_afterburner/js/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+
+	if( jQuery('#ggl-map').length > 0 ){
+	//google map api
+		var map;
+
+		function initialize() {
+			var mapOptions = {
+				zoom: 12,
+				center: new google.maps.LatLng(37.956, 23.710)
+			};
+			map = new google.maps.Map(document.getElementById('ggl-map'),
+				mapOptions);
+			console.log(map);
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+		
+
+	}
+
+
+	
+
+
+
+});
