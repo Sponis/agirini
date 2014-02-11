cddf463affc7c3a60a809ea1b12b8b439c920ff7
bardius <george.bardis@maynardmalone.com>
2014-02-11 21:57:38 +0000

    fixed the formating of the main.js file

--- templates/rt_afterburner/js/main.js
+++ templates/rt_afterburner/js/main.js
@@ -1,37 +1,33 @@
 jQuery(document).ready(function() {
 
 	//toggle show-hide
-		jQuery(".content-toggler").hide();
-		jQuery(".toggle-trigger").click(function(event) {
-			event.preventDefault();
-		});
-		jQuery(".toggle-trigger").click(function() {
-			jQuery(".content-toggler").toggle();
-		});
+	jQuery(".content-toggler").hide();
 	
+	jQuery(".toggle-trigger").click(function(event) {
+		event.preventDefault();
+	});
+	
+	jQuery(".toggle-trigger").click(function() {
+		jQuery(".content-toggler").toggle();
+	});
 
-	if( jQuery('#ggl-map').length > 0 ){
-	//google map api
+
+	if (jQuery('#ggl-map').length > 0) {
+		//google map api
 		var map;
 
 		function initialize() {
+			
 			var mapOptions = {
 				zoom: 14,
 				center: new google.maps.LatLng(36.431, 25.434)
 			};
-			map = new google.maps.Map(document.getElementById('ggl-map'),
-				mapOptions);
-		//	console.log(map);
+			
+			map = new google.maps.Map(document.getElementById('ggl-map'), mapOptions);
+			//	console.log(map);
 		}
 
 		google.maps.event.addDomListener(window, 'load', initialize);
-		
-
 	};
 
-	
-
-
-
 });
-
